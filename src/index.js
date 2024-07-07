4c080c384c35d34b42c636179a1228ca089e2bd3
Mytth <Mytth75@gmail.com>
2024-07-07 22:28:08 +0700

    init

---
+++ src/index.js
@@ -0,0 +1,112 @@
+import './style.css';
+
+document.addEventListener( 
+	"DOMContentLoaded", function () { 
+	const holes = 
+		document.querySelectorAll(".hole"); 
+	const startButton = 
+		document.getElementById("startButton"); 
+	const endButton = 
+		document.getElementById("endButton"); 
+	const scoreDisplay = 
+		document.getElementById("score"); 
+	const timerDisplay = 
+		document.getElementById("timer"); 
+
+	let timer; 
+	let score = 0; 
+	let countdown; 
+	let moleInterval; 
+	
+	// Set the initial state to game over 
+	let gameOver = true;
+  let lifecounter = Number(document.getElementById("life").innerText);
+
+	function comeout() { 
+		holes.forEach(hole => {
+      if (hole.classList.contains('mole')) {
+        hole.classList.remove('mole')
+        if (lifecounter > 0) {
+          lifecounter--;
+          document.getElementById("life").innerText = lifecounter;
+        } else {
+          document.getElementById("life").innerText = "0";
+          endGame();
+        }
+      }
+			// hole.classList.remove('mole'); 
+			hole.removeEventListener( 
+				'click', handleMoleClick); 
+		});
+    if (!gameOver) {
+      let random = holes[Math.floor(Math.random() * 16)]; 
+      random.classList.add('mole');
+      random.addEventListener('click', handleMoleClick); 
+    } 
+	} 
+
+	function handleMoleClick() { 
+		if (!gameOver) { 
+			score++; 
+			scoreDisplay.textContent = `Score: ${score}`; 
+		} 
+		this.classList.remove('mole'); 
+	} 
+
+	function startGame() { 
+		if (!gameOver) { 
+		
+			// Prevent starting the game 
+			// again if it's already in progress 
+			return; 
+		} 
+    
+		gameOver = false; 
+		score = 0; 
+		scoreDisplay.textContent = `Score: ${score}`; 
+		timer = 60; 
+		timerDisplay.textContent = `Time: ${timer}s`; 
+
+		startButton.disabled = true; 
+		endButton.disabled = false; 
+
+		countdown = setInterval(() => { 
+			timer--; 
+			timerDisplay.textContent = `Time: ${timer}s`; 
+
+			if (timer <= 0) { 
+				endGame()
+			} 
+		}, 1000); 
+
+		moleInterval = setInterval(() => { 
+			if (!gameOver) comeout(); 
+		}, 1000); 
+
+		console.log("Game started"); 
+	} 
+
+	function endGame() { 
+		clearInterval(countdown); 
+		clearInterval(moleInterval); 
+		gameOver = true; 
+		alert(`Game Ended!\nYour final score: ${score}`);
+		score = 0; 
+		timer = 60; 
+		scoreDisplay.textContent = `Score: ${score}`; 
+		timerDisplay.textContent = `Time: ${timer}s`; 
+		startButton.disabled = false; 
+		endButton.disabled = true;
+    holes.forEach(hole => {
+      hole.classList.remove('mole');
+      hole.removeEventListener(
+        'click', handleMoleClick);
+    });
+    lifecounter = 5;
+    document.getElementById("life").innerText = lifecounter;
+	} 
+
+	startButton.addEventListener("click", startGame); 
+	endButton.addEventListener("click", endGame); 
+});
+
---
+++ webpack.config.cjs
@@ -0,0 +1,46 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'dist', to: './' },
+      ],
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  target: 'web',
+  mode: 'development',
+};
